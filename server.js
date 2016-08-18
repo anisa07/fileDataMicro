37cb4b5721d8a9425bfed1ebde7a7977affda95c
Anisa <askany07@gmail.com>
2016-08-18 19:39:54 +0000

    see fileStamp

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var path = require('path');
+var express =   require("express");
+var multer  =   require('multer');
+var app     = express();
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
 
-var app = express();
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("{size : " + req.file.size + "}");
+    });
 });
 
 app.listen(8080, function () {
